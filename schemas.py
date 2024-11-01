895e817ee4e681936286af240e86ba91d83cf59e
nagashreepoojari <nagashreerpoojari@gmail.com>
2024-11-01 14:51:55 +0530

    add back pydantic models

---
+++ schemas.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class PostUpdate(PostBase):
+    pass
+
+
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
