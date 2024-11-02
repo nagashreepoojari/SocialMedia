53a8f2a48c523d7e7f9f7d86c7d439e787cbeb3d
nagashreepoojari <nagashreerpoojari@gmail.com>
2024-11-02 15:03:12 +0530

    Update oauth.py with verify access token and get user details method

--- app/oauth.py
+++ app/oauth.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
+from http.client import HTTPException
 
 import jwt
+from .models import User
+from .schemas import UserBase
 
 # secret key
 # algorith
@@ -30,3 +33,29 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+class CredentialException(Exception):
+    def __init__(self, message="Invalid or expired token"):
+        self.message = message
+        super().__init__(self.message)
+
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id = payload.get('user_id')
+        if id is None:
+            raise CredentialException()
+        current_user = User.query.get(id)
+        return current_user
+    except jwt.ExpiredSignatureError:
+        raise CredentialException("Token has expired")
+    except jwt.InvalidTokenError:
+        raise CredentialException("Invalid token")
+    except Exception as e:
+        raise CredentialException(f"An error occurred: {str(e)}")
+
+
+def get_current_user(token):
+    return verify_access_token(token)
