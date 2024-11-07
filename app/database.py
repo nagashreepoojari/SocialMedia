850cc8a3f1b0f2c8728e66196cf39330219dbb9b
nagashreepoojari <nagashreerpoojari@gmail.com>
2024-11-07 20:38:57 +0530

    use .env with pydantic validation

--- app/database.py
+++ app/database.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from .models import db
-
+from .config import settings
 app = Flask(__name__)
-
-DATABASE_URI = 'postgresql+psycopg2://postgres:Nagashree%40postgres@localhost:5432/flaskapi'
+password = settings.database_password
+print(password)
+DATABASE_URI = f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
