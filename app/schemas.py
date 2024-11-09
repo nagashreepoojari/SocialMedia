4b9057195180e3561021fd25efd8f97f73847268
nagashreepoojari <nagashreerpoojari@gmail.com>
2024-11-09 16:49:43 +0530

    logic to add votes to the posts

--- app/schemas.py
+++ app/schemas.py
@@ -3,28 +3,6 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class PostBase(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
-class PostCreate(PostBase):
-    pass
-
-
-class PostUpdate(PostBase):
-    pass
-
-
-class PostResponse(PostBase):
-    id: int
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
-
-
 class UserBase(BaseModel):
     email: str
     password: str
@@ -47,6 +25,42 @@ class UserResponse(BaseModel):
         from_attributes = True
 
 
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class PostUpdate(PostBase):
+    pass
+
+
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    votes: int
+
+    # owner: UserBase
+
+    class Config:
+        from_attributes = True
+
+
 class Login(BaseModel):
     email: str
     password: str
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: int
