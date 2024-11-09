a2bcf5eadc6930915158e6852b843bb55da0d71f
nagashreepoojari <nagashreerpoojari@gmail.com>
2024-11-09 16:49:18 +0530

    logic to add votes to the posts

--- app/models.py
+++ app/models.py
@@ -1,29 +1,9 @@
 import json
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
-
-
-class Post(db.Model):
-    __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    published = db.Column(db.Boolean, server_default='TRUE', nullable=False)
-    created_at = db.Column(db.DateTime, server_default=db.func.current_timestamp(), nullable=False)
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "content": self.content,
-            "published": self.published,
-            "created_at": self.created_at
-        }
-
-    def __repr__(self):
-        return json.dumps(self.to_dict())
 
 
 class User(db.Model):
@@ -38,6 +18,47 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             "created_at": self.created_at
+        }
+
+    def __repr__(self):
+        return json.dumps(self.to_dict())
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    published = db.Column(db.Boolean, server_default='TRUE', nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.current_timestamp(), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    votes = db.Column(db.Integer, server_default='0')
+    owner = relationship(User)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "published": self.published,
+            "created_at": self.created_at,
+            "owner_id": self.owner_id,
+            "votes": self.votes
+        }
+
+    def __repr__(self):
+        return json.dumps(self.to_dict())
+
+
+class Vote(db.Model):
+    __tablename__ = 'votes'
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), primary_key=True, nullable=False)
+
+    def to_dict(self):
+        return {
+            "post_id": self.post_id,
+            "user_id": self.user_id
         }
 
     def __repr__(self):
