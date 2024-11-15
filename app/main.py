7eb18b7414e52d9b9624ff2ffe8ec52d7915679a
nagashreepoojari <nagashreerpoojari@gmail.com>
2024-11-15 20:00:57 +0530

    Update main.py
    added alembic data migration tool to the app

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from flasgger import Swagger
+# from flasgger import Swagger
+from flask_alembic import Alembic
+from flask_migrate import Migrate
 
 from .database import app
 from .models import db
@@ -6,17 +8,22 @@ from .models import db
 from .routers.auth import auth_bp
 from .routers.post import posts_bp
 from .routers.user import users_bp
+from .routers.vote import votes_bp
 
-with app.app_context():
-    db.create_all()
+migrate = Migrate(app, db)
+# alembic = Alembic()
+# alembic.init_app(app)
 
-swagger = Swagger(app, template={
-    "info": {
-        "title": "My Flask API",
-        "description": "An example API using Flask and Swagger",
-        "version": "1.0.0"
-    }
-})
+# with app.app_context():
+#     db.create_all()
+
+# swagger = Swagger(app, template={
+#     "info": {
+#         "title": "My Flask API",
+#         "description": "An example API using Flask and Swagger",
+#         "version": "1.0.0"
+#     }
+# })
 
 
 @app.route("/")
@@ -27,3 +34,4 @@ def hello_world():
 app.register_blueprint(posts_bp, url_prefix='/posts')
 app.register_blueprint(users_bp, url_prefix='/users')
 app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(votes_bp, url_prefix='/vote')
